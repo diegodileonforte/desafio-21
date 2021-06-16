987630548da875f5be6ef12b3e5a911a0e43e593
Diego Di Leonforte <dileonfortediego@gmail.com>
2021-06-16 19:33:50 -0300

    Login-Logout Session

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ import { Server as IOServer } from 'socket.io'
 import router from './routes/productos.routes.js'
 import routerMsg from './routes/mensajes.routes.js'
 import mongoose from 'mongoose'
+
 import { normalize, schema } from "normalizr"
 import utils from "util"
+
+import session from 'express-session';
+import cookieParser from 'cookie-parser'
 
 import Mensaje from './controllers/Mensaje.js'
 const msgClass = new Mensaje()
@@ -30,10 +34,50 @@ app.use(express.static('public'))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 app.use('/productos', router)
 app.use('/mensajes', routerMsg)
 
+app.set('views', './public');
+app.set('view engine', 'ejs')
+
+app.use(session({
+    name: 'Desafio24',
+    secret: 'shhh',
+    resave: false,
+    saveUninitialized: false,
+    rolling: true,
+    cookie: {
+        maxAge: 60000,
+}}))
+
+app.get('/login', (req, res) => {
+    req.session.user = req.query.user
+    if (req.session.user) {
+        res.render('login', { name: req.session.user })
+        if (!req.session.user) {
+            res.redirect('/')
+        }
+    }
+    else {
+        res.send(`Error en el Login`)
+    }
+})
+
+app.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            res.json({ error: 'Error' })
+        } else {
+            return setTimeout(() => {
+                res.redirect('/')
+            }, 2000);
+        }
+    })
+})
+
+//Chat con Normalizr
 
 const chat = []
 
